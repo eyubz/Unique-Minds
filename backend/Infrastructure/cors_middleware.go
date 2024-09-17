2f0aa25e0be28a87135f288f62ca2f406467230a
eyerusalembezu15@gmail.com <eyerusalembezu15@gmail.com>
2024-09-17 23:20:49 +0300

    CORS middleware updated to allow connecting with production envirnment.

--- backend/Infrastructure/cors_middleware.go
+++ backend/Infrastructure/cors_middleware.go
@@ -21,7 +21,7 @@ func (c *Core) CORSMiddleware() gin.HandlerFunc {
     return func(ctx *gin.Context) {
         fmt.Println("Lopping through the CORS middleware")
         fmt.Println("CORS middleware executed for:", ctx.Request.Method, ctx.Request.URL.Path)
-        ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+        ctx.Writer.Header().Set("Access-Control-Allow-Origin", "https://unique-minds.onrender.com")
         ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
         ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
